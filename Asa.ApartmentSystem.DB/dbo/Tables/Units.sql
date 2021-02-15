c9458e08d6fdc3f8df973f061753e2937e0d9b7e
moxeed <mortezabahjat@gmail.com>
2021-02-15 09:43:08 +0330

    Junk Files Removed

--- Asa.ApartmentSystem.DB/dbo/Tables/Units.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Units]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [building_id] INT NOT NULL, 
-    [number] INT NOT NULL, 
-    [area] DECIMAL(5, 1) NOT NULL, 
-    [description] NVARCHAR(250) NULL, 
-    CONSTRAINT [Fk_unit_building] FOREIGN KEY ([building_id]) REFERENCES [building](id)
-)
