22359c1e4d7d5742e4bebbf45d8de83af20e8631
moxeed <59084542+moxeed@users.noreply.github.com>
2021-02-15 09:42:25 +0330

    Delete Asa.ApartmentSystem.DB directory

--- Asa.ApartmentSystem.DB/dbo/Stored Procedures/units_get_all.sql
+++
@@ -1,9 +0,0 @@
-﻿create procedure [dbo].[units_get_all]
-@building_id int
-as
-SELECT [Id],[building_id],[number] ,[area],[description]
-  FROM [dbo].[Units]
-  where [building_id]=@building_id
-GO
-
-
