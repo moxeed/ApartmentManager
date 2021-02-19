ccc4125949e51b3f2735e7de5c582304674ba43e
alireza asadi <ali.s.asadi98@gmail.com>
2021-02-19 20:38:02 +0330

    Expense repository added

---
+++ ASa.ApartmentManagement.Core/Interfaces/Repositories/IExpenseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Asa.ApartmentManagement.Core.ChargeCalculation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asa.ApartmentManagement.Core.Interfaces.Repositories
+{
+    public interface IExpenseRepository
+    {
+        Task<ICollection<Expens>> GetAllByDateAsync(DateTime from, DateTime to);
+    }
+}
---
+++ Asa.ApartmentManagement.persistence/Repositories/ExpenseRepository.cs
@@ -0,0 +1,27 @@
+﻿using Asa.ApartmentManagement.Core.ChargeCalculation;
+using Asa.ApartmentManagement.Core.Interfaces.Repositories;
+using Asa.ApartmentManagement.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asa.ApartmentManagement.Persistence.Repositories
+{
+    public class ExpenseRepository : IExpenseRepository
+    {
+        private ApplicationDbContext _applicationDbContext;
+
+        public ExpenseRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public Task<ICollection<Expens>> GetAllByDateAsync(DateTime from, DateTime to)
+        {
+
+            //TODO: GET all expenses in the That range of Time
+            throw new NotImplementedException();
+        }
+    }
+}
