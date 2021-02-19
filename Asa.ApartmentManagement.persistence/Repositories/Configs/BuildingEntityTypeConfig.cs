2d9a9a150ae9cd3f1289d20e01be9d86a2ca0194
alireza asadi <ali.s.asadi98@gmail.com>
2021-02-19 15:15:31 +0330

    configuration for buildingentitytype added

---
+++ Asa.ApartmentManagement.persistence/Repositories/Configs/BuildingEntityTypeConfig.cs
@@ -0,0 +1,18 @@
+﻿using Asa.ApartmentManagement.Core.ChargeCalculation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asa.ApartmentManagement.Persistence.Repositories.Configs
+{
+    public class BuildingEntityTypeConfig : IEntityTypeConfiguration<Building>
+    {
+            public void Configure(EntityTypeBuilder<Building> builder)
+            {
+                builder.HasKey(x => x.BuildingId);
+
+            }
+    }
+}
