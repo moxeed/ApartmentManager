c322d8624d50917a6b6a2afbc514ab47d65ad0ae
alireza asadi <ali.s.asadi98@gmail.com>
2021-02-19 20:18:31 +0330

    costomized attr added

---
+++ ASa.ApartmentManagement.Core/ChargeCalculation/Formulas/CalculationFormulaAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asa.ApartmentManagement.Core.ChargeCalculation.Formulas
+{
+
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+
+    sealed class CalculationFormulaAttribute : Attribute
+    {
+        private Enum v;
+
+        public CalculationFormulaAttribute(FormulaType v)
+        {
+            this.v = v;
+        }
+
+        public string FormulaTitle { get; internal set; }
+    }
+}
