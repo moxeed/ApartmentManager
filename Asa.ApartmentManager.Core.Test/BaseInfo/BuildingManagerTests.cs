a0780ebb06fc99461d743c088be446e1b8f46b4d
alireza asadi <ali.s.asadi98@gmail.com>
2021-02-28 23:25:08 +0330

    formula Tests added

--- Asa.ApartmentManager.Core.Test/BaseInfo/BuildingManagerTests.cs
+++ Asa.ApartmentManager.Core.Test/BaseInfo/BuildingManagerTests.cs
@@ -11,17 +11,18 @@ namespace Asa.ApartmentSystem.Core.Test
 {
     public class BuildingManagerTests
     {
-
+        FakeBuildingRepository _repository;
+        BuildingManager buildingManager;
         [SetUp]
         public void Setup()
         {
+            _repository = new FakeBuildingRepository();
+            buildingManager = new BuildingManager(_repository); 
         }
 
         [Test]
         public void Building_Name_Cannot_Be_Null_Or_Empty()
         {
-            FakeBuildingRepository _repository = new FakeBuildingRepository();
-            BuildingManager buildingManager = new BuildingManager(_repository);
             BuildingDto building = new BuildingDto { BuildingId = 0, Name = string.Empty, NumberOfUnits = 10 };
             Assert.CatchAsync(() => buildingManager.AddBuildingAsync(building));
         }
@@ -29,15 +30,21 @@ namespace Asa.ApartmentSystem.Core.Test
         [Test]
         public async Task Building_Added_Successfuly()
         {
-            FakeBuildingRepository _repository = new FakeBuildingRepository();
-            BuildingManager buildingManager = new BuildingManager(_repository);
-            BuildingDto building = new BuildingDto { BuildingId= 1, Name = "MyBuilding", NumberOfUnits = 10 };
+            BuildingDto building = new BuildingDto { BuildingId = 1, Name = "MyBuilding", NumberOfUnits = 10 };
             await buildingManager.AddBuildingAsync(building);
-            Console.WriteLine(building.BuildingId);
             Assert.AreEqual(1, building.BuildingId);
         }
 
-
+        [Test]
+        public async Task Buillding_Edit_Successfuly()
+        {
+            BuildingNameDto buildingname = new BuildingNameDto { BuildingId = 1, BuildingName = "Salam" };
+            BuildingDto building = new BuildingDto { BuildingId = 1, Name = "Khodafes", NumberOfUnits = 10 };
+            await buildingManager.AddBuildingAsync(building);
+            await buildingManager.EditBuldingNameAsync(buildingname);
+            var editedbuilding  = await _repository.GetBuildingAsync(building.BuildingId);
+            Assert.AreEqual("Salam", editedbuilding.Name);
+        }
         
     }
 }
